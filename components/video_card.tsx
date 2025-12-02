37996f4ba5167167b5a85fe81593a05ccd339613
Julia Bush <j.elizabethbush@gmail.com>
2025-12-03 00:28:00 +0100

    add: creating the video frame box

--- components/video_card.tsx
+++ components/video_card.tsx
@@ -6,5 +6,9 @@ interface VideoCradProps {
 }
 
 export default function VideoCard({ src, videRef }: VideoCardProps) {
-  return <div className="flex flex-col items-center"></div>;
+  return (
+    <div className="flex flex-col items-center">
+      <div className="w-[300px] h-[500px] rounded-xl overflow-hidden shadow-xl border-4 border-blue-300"></div>
+    </div>
+  );
 }
