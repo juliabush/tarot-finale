11cbe8e9a40fc9757ec4a9f9f6c9841ccdeb0a60
Julia Bush <j.elizabethbush@gmail.com>
2025-12-03 03:00:45 +0100

    fix: correct typos in VideoCardProps interface and prop name

--- components/video_card.tsx
+++ components/video_card.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-interface VideoCradProps {
+interface VideoCardProps {
   src: string;
   videoRef: React.Ref<HTMLVideoElement>;
 }
 
-export default function VideoCard({ src, videRef }: VideoCardProps) {
+export default function VideoCard({ src, videoRef }: VideoCardProps) {
   return (
     <div className="flex flex-col items-center">
       <div className="w-[300px] h-[500px] rounded-xl overflow-hidden shadow-xl border-4 border-blue-300">
